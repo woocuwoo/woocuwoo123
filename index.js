19a456e2ad3daa7b60b9afe4dd65b2cd152b3f79
woocuwoo <111623358+woocuwoo@users.noreply.github.com>
2023-03-13 21:17:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 
-server.get("/", (req, res) => {
+server.get("/시간표", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
